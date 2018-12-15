eedbc8a86b3f7d293135deb53f0a8e0fe3286c29
CMA <87302380@qq.com>
2018-12-16 00:12:29 +0100

    增加了砖块和角色的体积 完成了边缘的限制

--- js/Tile.js
+++ js/Tile.js
@@ -4,6 +4,17 @@ class TileResolver {
         this.tileSize = tileSize;
     }
 
+    toIndexRange(pos1, pos2){
+        var pMax = Math.ceil(pos2 / this.tileSize) * this.tileSize;
+        var range = [];
+        let pos = pos1;
+        do {
+            range.push(this.toIndex(pos));
+            pos += this.tileSize;
+        } while (pos < pMax);
+        return range;
+    }
+
     toIndex(pos){
         return Math.floor(pos / this.tileSize);
     }
@@ -11,27 +22,83 @@ class TileResolver {
     getByIndex(indexX, indexY){
         var tile = this.matrix.get(indexX, indexY);
         if (tile){
+            var x1 = indexX * this.tileSize;
+            var x2 = x1 + this.tileSize;
+            var y1 = indexY * this.tileSize;
+            var y2 = y1 + this.tileSize;
             return {
                 tile,
+                x1,
+                x2,
+                y1,
+                y2,
             };
         }
     }
 
-    matchByPosition(posX, posY){
+    serchByPosition(posX, posY){
         return this.getByIndex(this.toIndex(posX), this.toIndex(posY));
+    }
+
+    serchByRange(x1, x2, y1, y2){
+        var matches = [];
+        this.toIndexRange(x1, x2).forEach(indexX => {
+            this.toIndexRange(y1, y2).forEach(indexY => {
+                var match = this.getByIndex(indexX, indexY);
+                if (match){
+                    matches.push(match);
+                }
+            });
+        });
+        return matches;
     }
 }
 
-class Tile {
+class TileCollider {
     constructor(tileMatrix){
         this.tiles = new  TileResolver(tileMatrix);
     }
+    checkX(entity){
+        var matches = this.tiles.serchByRange(
+            entity.pos.x, entity.pos.x + entity.size.x,
+            entity.pos.y, entity.pos.y + entity.size.y);
+        matches.forEach(match => {
+            if (match.tile.name !== 'ground'){
+                return;
+            }
+            if (entity.vel.x > 0){
+                if (entity.pos.x + entity.size.x > match.x1){
+                    entity.pos.x = match.x1 - entity.size.x;
+                    entity.vel.x = 0;
+                }
+            }else if(entity.vel.x<0){
+                if (entity.pos.x < match.x2){
+                    entity.pos.x = match.x2;
+                    entity.vel.x = 0;
+                }
+            }
+        });
+    }
 
-    test(entity){
-        var match = this.tiles.matchByPosition(entity.pos.x, entity.pos.y);
-        if (match) {
-            console.log(match, match.tile);
-        }
-
+    checkY(entity){
+        var matches = this.tiles.serchByRange(
+            entity.pos.x, entity.pos.x + entity.size.x,
+            entity.pos.y, entity.pos.y + entity.size.y);
+        matches.forEach(match => {
+            if (match.tile.name !== 'ground'){
+                return;
+            }
+            if (entity.vel.y > 0){
+                if (entity.pos.y + entity.size.y > match.y1){
+                    entity.pos.y = match.y1 - entity.size.y;
+                    entity.vel.y = 0;
+                }
+            }else if(entity.vel.y<0){
+                if (entity.pos.y < match.y2){
+                    entity.pos.y = match.y2;
+                    entity.vel.y = 0;
+                }
+            }
+        });
     }
 }
